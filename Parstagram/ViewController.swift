b3b4df10c6021a3f5fd7d5fc3e829425349ec408
Jenny Li <wangjunqian@wangjunqiandeMacBook-Pro-2.local>
2019-03-17 18:57:20 -0400

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Jenny Li on 3/17/19.
+//  Copyright © 2019 Jenny Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
