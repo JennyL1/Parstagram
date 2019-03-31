7cd96f66994d85f5f7dfc2523b02799e0479bd66
Jenny Li <jenny.li6@baruchmail.cuny.edu>
2019-03-31 00:13:34 -0400

    update missing files

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,26 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Jenny Li on 3/30/19.
+//  Copyright © 2019 Jenny Li. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
